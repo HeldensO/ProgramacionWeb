30913633ca58e31359aa0f13ae6c1368d3d32c38
Fernando Sinner <fersinner13@gmail.com>
2023-02-06 10:19:55 -0300

    ultimos cambios

---
+++ JavaScript/do-while.js
@@ -0,0 +1,7 @@
+
+
+var x = 1;
+do {
+    console.log("valor de x: "+x)
+    x++;
+} while (x<=50);
---
+++ JavaScript/tipo-de-variable.js
@@ -0,0 +1,5 @@
+let numero;       //defino la variable
+
+numero = 5;       //le asigno un valor numerico
+
+numero = "John";  //le asigno el valor string de john a x
